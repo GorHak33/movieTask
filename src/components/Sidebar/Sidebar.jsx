c19c90b47674a4645c5583da5ac1d03345a319df
GorHak33 <david@eprc.co.il>
2023-11-15 06:10:14 +0400

    sidebar hover effect

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import styles from "./Sidebar.module.css";
+import React, {useState} from "react";
+import styles from "./Sidebar.module.css"; // Import module CSS
 import SearchIcon from "../../assets/icons/SearchIcon.png";
 import MovieIcon from "../../assets/icons/MovieIcon.png";
 import GenresIcon from "../../assets/icons/GenresIcon.png";
@@ -8,74 +8,67 @@ import WatchLaterIcon from "../../assets/icons/WatchLaterIcon.png";
 import Icon from "../../assets/icons/Icon.png";
 
 const Sidebar = () => {
-  const [activeItem, setActiveItem] = useState("");
+    const [backgroundBlockDisplay, setBackgroundBlockDisplay] = useState('none')
 
-  const handleItemClick = item => {
-    setActiveItem(item);
-  };
-
-  return (
-    <div className={styles.sidebar}>
-      <div className={styles.menu}>
-        <ul className={styles.mainMenu}>
-          <li
-            className={activeItem === "search" ? styles.active : ""}
-            onClick={() => handleItemClick("search")}
-          >
-            <img src={SearchIcon} alt="Search Icon" className={styles.icon} />
-            <span className={styles.text}>Search</span>
-          </li>
-          <li
-            className={activeItem === "home" ? styles.active : ""}
-            onClick={() => handleItemClick("home")}
-          >
-            <img src={HomeIcon} alt="Home Icon" className={styles.icon} />
-            <span className={styles.text}>Home</span>
-          </li>
-          <li
-            className={activeItem === "tvShows" ? styles.active : ""}
-            onClick={() => handleItemClick("tvShows")}
-          >
-            <img src={Icon} alt="TV Shows Icon" className={styles.icon} />
-            <span className={styles.text}>TV Shows</span>
-          </li>
-          <li
-            className={activeItem === "movies" ? styles.active : ""}
-            onClick={() => handleItemClick("movies")}
-          >
-            <img src={MovieIcon} alt="Movies Icon" className={styles.icon} />
-            <span className={styles.text}>Movies</span>
-          </li>
-          <li
-            className={activeItem === "genres" ? styles.active : ""}
-            onClick={() => handleItemClick("genres")}
-          >
-            <img src={GenresIcon} alt="Genres Icon" className={styles.icon} />
-            <span className={styles.text}>Genres</span>
-          </li>
-          <li
-            className={activeItem === "watchLater" ? styles.active : ""}
-            onClick={() => handleItemClick("watchLater")}
-          >
-            <img
-              src={WatchLaterIcon}
-              alt="Watch Later Icon"
-              className={styles.icon}
-            />
-            <span className={styles.text}>Watch Later</span>
-          </li>
-        </ul>
-        <div className={styles.profileInfo}>
-          <div className={styles.profileIcons}>{/* Profile icons */}</div>
-          <ul className={styles.additionalMenu}>
-            <li>Language</li>
-            <li>Get Help</li>
-            <li>Exit</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <div
+                style={{
+                    top: 0,
+                    left: 0,
+                    width: '100vw',
+                    height: '100vh',
+                    display: backgroundBlockDisplay,
+                    position: "absolute",
+                    zIndex: 9999,
+                    backgroundColor: 'rgba(0, 0, 0, 0.8)'
+                }}/>
+            <div className={styles.sidebar}
+                 onMouseMove={
+                     backgroundBlockDisplay === 'none' ? () => setBackgroundBlockDisplay('block') : (() => {
+                     })
+                 }
+                 onMouseLeave={() => setBackgroundBlockDisplay('none')}>
+                <div className={styles.menu}>
+                    <ul className={styles.mainMenu}
+                    >
+                        <li>
+                            <img src={SearchIcon} alt="Search Icon" className={styles.icon}/>
+                            <span className={styles.text}>Search</span>
+                        </li>
+                        <li>
+                            <img src={HomeIcon} alt="Search Icon" className={styles.icon}/>
+                            <span className={styles.text}>Home</span>
+                        </li>
+                        <li>
+                            <img src={Icon} alt="Search Icon" className={styles.icon}/>
+                            <span className={styles.text}>TV Shows</span>
+                        </li>
+                        <li>
+                            <img src={MovieIcon} alt="Search Icon" className={styles.icon}/>
+                            <span className={styles.text}>Movies</span>
+                        </li>
+                        <li>
+                            <img src={GenresIcon} alt="Search Icon" className={styles.icon}/>
+                            <span className={styles.text}>Genres</span>
+                        </li>
+                        <li>
+                            <img src={WatchLaterIcon} alt="Icon" className={styles.icon}/>
+                            <span className={styles.text}>Watch Later</span>
+                        </li>
+                    </ul>
+                    <div className={styles.profileInfo}>
+                        {/*<div className={styles.profileIcons}>/!* Profile icons *!/</div>*/}
+                        <ul className={styles.additionalMenu}>
+                            <li>Language</li>
+                            <li>Get Help</li>
+                            <li>Exit</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Sidebar;
